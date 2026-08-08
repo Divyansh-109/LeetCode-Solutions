c1b1cdd5c841d1b49cf9693d403cf4d7b38f42f4
Divyansh Chauhan <divyanshchauhan1352@gmail.com>
2026-08-08 18:40:48 +0000

    Sync LeetCode submission - Merge k Sorted Lists - Runtime - 4 ms (42.49%), Memory - 18.5 MB (48.64%)

---
+++ leetcode-problems/0023-merge-k-sorted-lists/solution.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()){
+            return nullptr;
+        }
+
+        int interval = 1;
+        while(interval < lists.size()){
+            for (int i = 0; i + interval < lists.size(); i += interval*2){
+                lists[i] = mergeTwoLists(lists[i], lists[i + interval]);
+            }
+            interval *= 2;
+        };
+        return lists[0];
+    }
+private:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
+        ListNode* dummy = new ListNode(0);
+        ListNode* curr = dummy;
+
+        while(l1 && l2){
+            if (l1 -> val <= l2 -> val){
+                curr -> next = l1;
+                l1 = l1 -> next;
+            } else {
+                curr -> next = l2;
+                l2 = l2 -> next;
+            }
+            curr = curr -> next;
+        };
+        if (l1){
+            curr -> next = l1;
+        }
+        if (l2){
+            curr -> next = l2;
+        }
+        ListNode* result = dummy -> next;
+        delete dummy;
+        return result;
+    }
+};
